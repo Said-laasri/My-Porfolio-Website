02f77cfd8aad7f26c7cb117db58a987283ca2e6b
Said-laasri <100702612+Said-laasri@users.noreply.github.com>
2022-06-12 11:08:55 +0100

    Click mobile menu function

--- menu.js
+++ menu.js
@@ -1,6 +1,10 @@
 let menu = document.querySelector("#side-bar");
 let menubar= document.querySelector(".portfolio");
 let closemenu=document.querySelector(".menu-close");
+let worksection=document.querySelector("#jumptopagework ");
+let aboutsection=document.querySelector("#jumptopageabout");
+let contactsection=document.querySelector("#jumptopagecontact");
+
 menu.addEventListener('click',function(){
     menubar.classList.toggle("nemubar");
     menu.style.visibility="hidden";
@@ -8,6 +12,24 @@ menu.addEventListener('click',function(){
 }
 )
 closemenu.addEventListener('click',function(){
+    menubar.classList.remove("nemubar");
+    menu.style.visibility="visible";
+    closemenu.style.visibility="hidden";
+}
+)
+worksection.addEventListener('click',function(){
+    menubar.classList.remove("nemubar");
+    menu.style.visibility="visible";
+    closemenu.style.visibility="hidden";
+}
+)
+aboutsection.addEventListener('click',function(){
+    menubar.classList.remove("nemubar");
+    menu.style.visibility="visible";
+    closemenu.style.visibility="hidden";
+}
+)
+contactsection.addEventListener('click',function(){
     menubar.classList.remove("nemubar");
     menu.style.visibility="visible";
     closemenu.style.visibility="hidden";
