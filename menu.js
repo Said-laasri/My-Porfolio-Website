36a54ea111d6a5a1fef9587ea0ecff9d5cbdebca
Tushar <singhtushar348@gmail.com>
2022-06-13 09:57:05 +0100

    fix eslint errors and files

--- menu.js
+++ menu.js
@@ -1,37 +1,41 @@
-let menu = document.querySelector("#side-bar");
-let menubar= document.querySelector(".portfolio");
-let closemenu=document.querySelector(".menu-close");
-let worksection=document.querySelector("#jumptopagework ");
-let aboutsection=document.querySelector("#jumptopageabout");
-let contactsection=document.querySelector("#jumptopagecontact");
+const menu = document.querySelector("#side-bar");
+const menubar = document.querySelector(".portfolio");
+const closemenu = document.querySelector(".menu-close");
+const worksection = document.querySelector("#jumptopagework ");
+const aboutsection = document.querySelector("#jumptopageabout");
+const contactsection = document.querySelector("#jumptopagecontact");
 
 menu.addEventListener('click',function(){
     menubar.classList.toggle("nemubar");
-    menu.style.visibility="hidden";
-    closemenu.style.visibility="visible";
+    menu.style.visibility = "hidden";
+    closemenu.style.visibility = "visible";
 }
-)
+);
+
 closemenu.addEventListener('click',function(){
     menubar.classList.remove("nemubar");
-    menu.style.visibility="visible";
-    closemenu.style.visibility="hidden";
+    menu.style.visibility = "visible";
+    closemenu.style.visibility = "hidden";
 }
-)
+);
+
 worksection.addEventListener('click',function(){
     menubar.classList.remove("nemubar");
-    menu.style.visibility="visible";
-    closemenu.style.visibility="hidden";
+    menu.style.visibility = "visible";
+    closemenu.style.visibility = "hidden";
 }
-)
+);
+
 aboutsection.addEventListener('click',function(){
     menubar.classList.remove("nemubar");
-    menu.style.visibility="visible";
-    closemenu.style.visibility="hidden";
+    menu.style.visibility = "visible";
+    closemenu.style.visibility = "hidden";
 }
-)
+);
+
 contactsection.addEventListener('click',function(){
     menubar.classList.remove("nemubar");
-    menu.style.visibility="visible";
-    closemenu.style.visibility="hidden";
+    menu.style.visibility = "visible";
+    closemenu.style.visibility = "hidden";
 }
-)
+);
