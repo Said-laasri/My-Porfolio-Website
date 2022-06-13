680cb4e07fa31f4e58c9cdacf70f5044b85e91ff
Tushar <singhtushar348@gmail.com>
2022-06-13 10:07:16 +0100

    fix eslint file

--- menu.js
+++ menu.js
@@ -1,41 +1,41 @@
-const menu = document.querySelector("#side-bar");
-const menubar = document.querySelector(".portfolio");
-const closemenu = document.querySelector(".menu-close");
-const worksection = document.querySelector("#jumptopagework ");
-const aboutsection = document.querySelector("#jumptopageabout");
-const contactsection = document.querySelector("#jumptopagecontact");
+const menu = document.querySelector('#side-bar');
+const menubar = document.querySelector('.portfolio');
+const closemenu = document.querySelector('.menu-close');
+const worksection = document.querySelector('#jumptopagework');
+const aboutsection = document.querySelector('#jumptopageabout');
+const contactsection = document.querySelector('#jumptopagecontact');
 
-menu.addEventListener('click',function(){
-    menubar.classList.toggle("nemubar");
-    menu.style.visibility = "hidden";
-    closemenu.style.visibility = "visible";
+menu.addEventListener('click', ()=> {
+  menubar.classList.toggle('nemubar');
+  menu.style.visibility = 'hidden';
+  closemenu.style.visibility = 'visible';
 }
 );
 
-closemenu.addEventListener('click',function(){
-    menubar.classList.remove("nemubar");
-    menu.style.visibility = "visible";
-    closemenu.style.visibility = "hidden";
+closemenu.addEventListener('click', ()=> {
+  menubar.classList.remove('nemubar');
+  menu.style.visibility = 'visible';
+  closemenu.style.visibility = 'hidden';
 }
 );
 
-worksection.addEventListener('click',function(){
-    menubar.classList.remove("nemubar");
-    menu.style.visibility = "visible";
-    closemenu.style.visibility = "hidden";
+worksection.addEventListener('click', ()=> {
+  menubar.classList.remove('nemubar');
+  menu.style.visibility = 'visible';
+  closemenu.style.visibility = 'hidden';
 }
 );
 
-aboutsection.addEventListener('click',function(){
-    menubar.classList.remove("nemubar");
-    menu.style.visibility = "visible";
-    closemenu.style.visibility = "hidden";
+aboutsection.addEventListener('click', ()=> {
+  menubar.classList.remove('nemubar');
+  menu.style.visibility = 'visible';
+  closemenu.style.visibility = 'hidden';
 }
 );
 
-contactsection.addEventListener('click',function(){
-    menubar.classList.remove("nemubar");
-    menu.style.visibility = "visible";
-    closemenu.style.visibility = "hidden";
+contactsection.addEventListener('click', ()=> {
+  menubar.classList.remove('nemubar');
+  menu.style.visibility = 'visible';
+  closemenu.style.visibility = 'hidden';
 }
 );
