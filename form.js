22df1c3a73f1892b2173ccf8cdc48574ad6a13c7
Said-laasri <100702612+Said-laasri@users.noreply.github.com>
2022-06-16 11:13:50 +0100

    Form-validation form.js

---
+++ form.js
@@ -0,0 +1,27 @@
+const form = document.getElementById('form1');
+const email = document.getElementById('email');
+const message = document.createElement('small');
+
+message.classList.add('error-message');
+form.appendChild(message);
+
+const emailValidation = (email) => {
+  const regularEx = /^([a-z\d-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
+  return regularEx.test(email.value.trim());
+};
+
+function emailChecker() {
+  const emailValues = email.value.trim();
+
+  if (emailValues === '') {
+    message.textContent = 'please add your email';
+  } else if (!emailValidation(email)) {
+    message.textContent = 'Please Enter your email adress with lowerCase!!';
+  } else {
+    message.textContent = 'Succes';
+  }
+}
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  emailChecker();
+});
